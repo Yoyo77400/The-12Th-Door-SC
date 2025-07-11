fe563ccccedafa780ff49c8e3c46f4975cb0e5ec
Yohan <yohan.georgelin@gmail.com>
2025-07-11 16:24:54 +0200

    add MatchNFT contract

--- src/FanToken.sol
+++ src/FanToken.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FanToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint 1 million tokens to the deployer
+    }
+}
--- src/FidelityNFT.sol
+++ src/FidelityNFT.sol
@@ -1,2 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MatchNFT is ERC721 {
+    uint256 public nextTokenId;
+    address public admin;
+    bool public marketEnabled;
+
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+        admin = msg.sender;
+        marketEnabled = false;
+    }
+
+    function mint(address to) external {
+        require(msg.sender == admin, "Only admin can mint");
+        _safeMint(to, nextTokenId);
+        nextTokenId++;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "https://api.example.com/metadata/";
+    }
+}
